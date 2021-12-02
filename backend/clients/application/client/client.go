4274cf9df128a0270da3fe8f8e60fb439e0a8e17
jalexanderII <a.joeljr@hotmail.com>
2021-12-02 16:49:23 -0500

    starting move to microservice over grpc --applications

--- backend/clients/application/client/client.go
+++ backend/clients/application/client/client.go
@@ -1,1 +1,21 @@
 package client
+
+import (
+	"log"
+
+	applicationpb "github.com/jalexanderII/solid-pancake/gen/application"
+	"google.golang.org/grpc"
+)
+
+func SetupClient() (applicationpb.ApplicationClient, *grpc.ClientConn) {
+	var opts []grpc.DialOption
+	opts = append(opts, grpc.WithInsecure())
+	opts = append(opts, grpc.WithBlock())
+
+	conn, err := grpc.Dial("localhost:10000", opts...)
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+
+	return applicationpb.NewApplicationClient(conn), conn
+}
